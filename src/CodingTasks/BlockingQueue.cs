adeab3f70b7c287c13dd7e5c216d9ad15ccfd022
vlad <thesleemer@icloud.com>
2017-10-14 16:13:36 +0200

    docs(BlockingQueue): fix comment  formating

--- src/CodingTasks/BlockingQueue.cs
+++ src/CodingTasks/BlockingQueue.cs
@@ -4,14 +4,20 @@ using System.Threading;
 
 namespace CodingTasks
 {
-    // A thread-safe blocking Queue of generic itmes.
+    /// <summary>
+    /// A thread-safe blocking Queue of generic itmes.
+    /// </summary>
+    /// <typeparam name="T">The type of the item in the queue</typeparam>
     public sealed class BlockingQueue<T>
     {
         private readonly Queue<T> _queue = new Queue<T>();
         private readonly object _barier = new object();                                              // Used to sync access to the queue from concurrent threads
         private readonly ManualResetEventSlim _newObjectAvailable = new ManualResetEventSlim(false); // Used to awake threads that are blocked on Pop
 
-        // Adds item to the tail of the queue.
+        /// <summary>
+        /// Adds item to the tail of the queue.
+        /// </summary>
+        /// <typeparam name="T">The type of the item to push to the queue</typeparam>
         public void Push(T item)
         {
             lock (_barier) {
@@ -20,8 +26,13 @@ namespace CodingTasks
             }
         }
 
-        // Removes and returns the item at the head of the queue. 
-        // If the queue's empty, this method blocks calling thread until new item added to the queue.  
+        /// <summary>
+        /// Removes and returns the item at the head of the queue.
+        /// </summary>
+        /// <typeparam name="T">The type of the item to pop from the queue</typeparam>
+        /// <remarks>
+        /// If the queue's empty, this method blocks calling thread until new item added to the queue.  
+        /// </remarks>
         public T Pop()
         {
             while (true) {
